bbd1ce72cd9061cedb5eff7525a3872a5f438f52
ronantimmons <ronantimmons1988@gmail.com>
2018-04-05 11:45:53 +0100

    added requirements.txt

---
+++ orders/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render,redirect
+from django.core.urlresolvers import reverse
+from .models import OrderItem
+from .forms import OrderCreateForm
+from .tasks import order_created
+from cart.cart import Cart
+
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order,
+                                         product=item['product'],
+                                         price=item['price'],
+                                         quantity=item['quantity'])
+            # clear the cart
+            cart.clear()
+            # launch asynchronous task
+            order_created.delay(order.id)
+            # set the order in the session
+            request.session['order_id'] = order.id
+            # redirect to the payment
+            return redirect(reverse('payment:process'))
+    else:
+        form = OrderCreateForm()
+    return render(request, 'orders/order/create.html', {'cart': cart,
+                                                        'form': form})
